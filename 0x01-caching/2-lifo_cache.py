1c6a520c33e9623b1aaf3f9c6df47e982507d756
FairTee <stephenodjidja7@gmail.com>
2024-05-22 13:14:38 +0000

    Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -19,37 +19,31 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """Initialize the LIFOCache."""
         super().__init__()
-        self.order = []
 
     def put(self, key, item):
-        """
-        Assign the item value to the key key in self.cache_data.
-
-        If key or item is None, this method does nothing.
-        If the cache exceeds MAX_ITEMS, the most recently added item is discarded.
+        """_summary_
 
         Args:
-            key (str): The key to store in the cache.
-            item (any): The item to store in the cache.
+                        key (_type_): _description_
+                        item (_type_): _description_
         """
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                self.order.remove(key)
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                # delete the last item in the dictionary
+                last_key, last_value = self.cache_data.popitem()
+                print("DISCARD: {}". format(last_key))
+
             self.cache_data[key] = item
-            self.order.append(key)
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                last_in_key = self.order.pop()
-                del self.cache_data[last_in_key]
-                print(f"DISCARD: {last_in_key}")
 
     def get(self, key):
-        """
-        Return the value in self.cache_data linked to key.
+        """return the value in self.cache_data linked to key
 
         Args:
-            key (str): The key to retrieve from the cache.
-
-        Returns:
-            The value associated with key if it exists, None otherwise.
+                        key (_type_): _description_
         """
+        if key is None or key not in self.cache_data.keys():
+            return None
         return self.cache_data.get(key)
